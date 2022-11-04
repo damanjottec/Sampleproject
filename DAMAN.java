68f1566030196794e12ce0d2385ae36f4e0cfc36
DAMAN <damanjotsingh16@gmail.com>
2022-11-04 20:20:08 +0530

    first commit

---
+++ DAMAN.java
@@ -0,0 +1,10 @@
+package in.ineuron.main.DAMAN;
+
+public class DAMAN {
+
+	public static void main(String[] args) {
+		System.out.println("DAMAN");
+
+	}
+
+}
---
+++ SINGH.java
@@ -0,0 +1,11 @@
+package in.ineuron.main.DAMAN;
+
+public class SINGH {
+
+	public static void main(String[] args) {
+		System.out.println("This is MR.Singh ");
+	
+
+	}
+
+}
